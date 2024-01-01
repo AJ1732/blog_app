d33fc9d8b899ed8c4b1bf6dbdd19b249e06d0dd5
AJ1732 <ejemeniboi@gmail.com>
2024-01-01 20:34:06 +0100

    update

--- index.js
+++ index.js
@@ -2,25 +2,41 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
-let post = ``;
+const port = 3001;
+// let post_title = ``;
+// let post_content = ``;
+// let post_date = ``;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const postGenerator = (req, res, next) => {
-  post = `${req.body.blog_post_input}`;
-  next();
-}
+// const postGenerator = (req, res, next) => {
+//   post_title = `${req.body.blog_post_title}`;
+//   post_content= `${req.body.blog_post_content}`;
+//   post_date= `${req.body.blog_post_date}`;
+//   next();
+// }
 
-app.use(postGenerator);
+// app.use(postGenerator);
 
 app.get("/", (req, res) => {
   res.render("index.ejs")
 });
 
 app.post("/submit", (req, res) => {  
-  res.render("index.ejs", { post })
+  let postTitle = `${req.body.blog_post_title}`;
+  let postContent= `${req.body.blog_post_content}`;
+  let postDate= `${req.body.blog_post_date}`;
+
+  console.log(postTitle);
+  console.log(postContent);
+  console.log(postDate);
+
+  res.render("index.ejs", { 
+    title: postTitle, 
+    content: postContent, 
+    date: postDate, 
+  });
 });
 
 app.listen(port, () => {
